c6db87fceec6d4ca8e9949015ef1c37e6478bc28
Greg Olmschenk <greg@olmschenk.com>
2022-06-20 21:54:39 -0400

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+//! Blazingly fast bogosort because Rust.
 extern crate rand;
 
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 
+/// Blazingly fast bogosort because Rust.
 pub fn bogosort(mut vector: Vec<i32>) -> Vec<i32> {
     while !is_sorted(&vector) {
         vector.shuffle(&mut thread_rng())
