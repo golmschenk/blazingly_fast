2b755e4509337a86432f8b700f6dbb5d2a321c69
Greg Olmschenk <greg@olmschenk.com>
2022-06-20 21:35:17 -0400

    Add bogosort

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+extern crate rand;
+
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+fn bogosort(mut vector: Vec<i32>) -> Vec<i32> {
+    while !is_sorted(&vector) {
+        vector.shuffle(&mut thread_rng())
+    }
+    vector
+}
+
+fn is_sorted(vector: &Vec<i32>) -> bool {
+    vector.windows(2).all(|window| window[0] <= window[1])
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::bogosort;
+
+    #[test]
+    fn it_works() {
+        let vector = vec![5, 3, 1, 2, 4];
+        let sorted_vector = bogosort(vector);
+        assert_eq!(sorted_vector, vec![1, 2, 3, 4, 5]);
+    }
+}
